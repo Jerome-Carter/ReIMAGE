e398ad668bda8af474e09b783cbf1211db303205
Jerome Carter <jeromecarter0722@gmail.com>
2020-04-06 04:36:09 -0400

    Updated project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tmp
+# ReIMAGE
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/tmp'),
+      dir: require('path').join(__dirname, './coverage/ReIMAGE'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
